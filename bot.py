dabcc7e6127ab742062e380886fff234db9f237b
rkrohk <rkakar2000@gmail.com>
2020-12-31 17:16:39 +0530

    New image

--- bot.py
+++ bot.py
@@ -15,8 +15,10 @@ logging.basicConfig(level=logging.DEBUG,
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+# Resolve BOT API KEY from environment variable
 updater = Updater(os.environ.get("BOT_API_KEY"), use_context=True)
 
+# Resolve BOT LOG CHAT from environment variable
 BOT_LOG_CHAT = os.environ.get("BOT_LOG_CHAT")
 
 
